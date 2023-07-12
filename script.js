8976a97df8c7c85a3afe609748966230212614bb
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-07-12 13:15:06 +0200

    criado script de button play again

--- script.js
+++ script.js
@@ -1,37 +1,57 @@
 const hands = document.querySelectorAll('.inside');
 const yourPicked = document.querySelector('.yourPicked');
+const closerBut = document.getElementById('close-but');
+const rules = document.querySelector('.rules');
+const handsDis = document.querySelector('.selectHand');
+const resultDis = document.querySelector('.result');
+cont rock = 
+// class ObjHands{
+//     constructor(id,st,es){
+//         this.id = id;
+//         this.styl = st;
+//         this.estru = yourPicked.classList.add('but-select');
+//     }
+// }
 
-class ObjHands{
-    constructor(id,st,es){
-        this.id = id;
-        this.styl = st;
-        this.estru = yourPicked.classList.add('but-select');
-    }
+function selectHand(){
+    resDisplay();
+    yourPicked.classList.remove('but-div');
+    yourPicked.classList.add('but-select'); 
+    yourPicked.innerHTML = `<div class="insideRes">
+    <img src="images/icon-rock.svg" alt="">
+  </div>`
 }
-
 // button Rules-----------------------------------------------------------
 document.getElementById('rulesbut').addEventListener('click',()=>{
-    let rules = document.querySelector('.rules');
     rules.style.display = 'block';
-    document.getElementById('close-but').addEventListener('click',()=>{
+    closerBut.addEventListener('click',()=>{
         rules.style.display = 'none';
     });
 });
 const resDisplay = ()=>{
-    setTimeout(()=>{document.querySelector('.result').style.display = 'flex';
-    document.querySelector('.selectHand').style.display = 'none';}, 100);
+    setTimeout(()=>{
+    resultDis.style.display = 'flex';
+    handsDis.style.display = 'none';}, 100);
     clearTimeout();
-    
-}
+};
+
+// Play Again---------------------------------------------------------
+
+document.getElementById('butAgain').addEventListener('click',()=>{
+    handsDis.style.display = 'block';
+    resultDis.style.display = 'none';
+})
 hands.forEach((hand)=>{
     hand.addEventListener('click', ()=>{
        switch(hand.id){
         case 'paper':
-            resDisplay();
+            selectHand();     
         break;
         case 'sciss':
+            selectHand();
         break;
         case 'rock':
+            selectHand();
         break;
        }
     });
