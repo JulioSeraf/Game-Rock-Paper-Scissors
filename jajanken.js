cb3822e7e184a2d1c1f6ec784b2067835b344417
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-07-25 17:22:29 +0200

    CRIANDO SCRIPT DE JAJANKEN

---
+++ jajanken.js
@@ -0,0 +1,18 @@
+const hands = document.querySelectorAll('.inside');
+hands.forEach((hand)=>{
+    hand.addEventListener('click',()=>{
+        let homeHand = Math.random();
+        console.log(homeHand)
+        switch(hand.id){
+            case 'rock':
+    
+            break;
+            case 'paper':
+
+            break;
+            case 'scinss':
+
+            break;
+        }
+    })
+})
